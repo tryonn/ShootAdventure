4b28cdfa6d2dbc63af3e8852a84bcb5abbc516c8
Simão Menezes <simaomenezesneto@gmail.com>
2015-10-10 09:40:39 -0300

    obstacle placement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private Transform tilePrefab;
@@ -9,6 +9,17 @@ public class MapGenerator : MonoBehaviour
     [Range(0,1)]
     [SerializeField] private float outlinePercent;
 
+    // ######## atributos para os obstaculos
+
+    [SerializeField] private List<Coord> allTileCoords;// lista que recebe os obstaculos
+    [SerializeField] private Queue<Coord> shuffledTileCoords; //
+    [SerializeField] private int seed = 10;
+    [SerializeField] private Transform obstaclePrefab;
+    [SerializeField] private int obstacleCount = 10;
+
+
+    // ######## atributos para os obstaculos
+
     private void Start()
     {
         MapGenerate();
@@ -16,6 +27,21 @@ public class MapGenerator : MonoBehaviour
 
     public void MapGenerate()
     {
+        // ####### begin - implementação dos obstaculos
+
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coord>(Utilitys.ShuffleArray(allTileCoords.ToArray(), seed));
+
+
+        // ####### and - implementação dos obstaculos
+
         string holdname = "Generated Map";
         if (transform.FindChild(holdname))
         {
@@ -28,11 +54,43 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x,y); // coordenadas para os obstaculos
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = mapHolder;
             }
+        }
+
+        // dados para os obstaculos
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+            newObstacle.parent = mapHolder;
+        }
+    }
+
+    private Vector3 CoordToPosition(int x, int y) {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    // pegando random para obstaculos
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+    // class para os obstaculos
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y) {
+            x = _x;
+            y = _y;
         }
     }
 }
